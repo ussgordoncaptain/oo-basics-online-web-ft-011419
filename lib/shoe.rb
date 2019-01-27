468c804a4947cf8e4f95a4ab1c72d77556fc003e
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-27 05:08:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,8 +3,8 @@ class Shoe
   def intialize(brand)
     @brand=brand
   end
-  attr_reader :brand, :condition
-  attr_accessor :color, :size, :material
+  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition
   def cobble
     puts "Your shoe is as good as new!"
     @condition = 
